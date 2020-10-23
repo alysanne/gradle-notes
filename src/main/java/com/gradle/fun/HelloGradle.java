eb51b049e7d0367acb611f1edcd6c66106e7aa74
Natalia Fernández <natalia.fpintos@gmail.com>
2020-10-23 17:52:03 +0100

    Add main Gradle files and tiny Java app

---
+++ src/main/java/com/gradle/fun/HelloGradle.java
@@ -0,0 +1,9 @@
+package com.gradle.fun;
+
+// Run build task and then run code with:
+// java -cp build/classes/java/main/ com.gradle.fun.HelloGradle
+public class HelloGradle {
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle, from Java.");
+    }
+}
